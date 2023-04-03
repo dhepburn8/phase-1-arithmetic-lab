653615dc893bc9a78bb6b52ba8ac8dfe938affe5
Dexter Hepburn <dhepburn8@gmail.com>
2023-04-03 01:43:59 -0700

    add

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
-
-// Write your code here
+{let num1 = 31
+let num2 = 2
+let multiply = num1 * num2 
+}
+{let num3 = 16
+let num4 = 4
+let mod = 16/4
+}
+{let random = 5
+Math.floor(Math.random() * 10) + 1;
+}
+{let max =
+Math.max(2, 4, 6, 8, 10, 16, 20)
+}
